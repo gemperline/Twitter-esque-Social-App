8b5989ee2e9551b6c1a14b4f401a40e5aef7dc08
gemperline <39207952+gemperline@users.noreply.github.com>
2020-05-20 16:15:56 -0700

    Update admin.js

--- gatherApp/functions/util/admin.js
+++ gatherApp/functions/util/admin.js
@@ -1,11 +1,7 @@
 const admin = require('firebase-admin');
 
 //~~~~~~~~~~ DO NOT PUBLISH admin.json in public repository ~~~~~~~~~~
-const serviceAccount = require('../key/admin');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://gather-app-af8a4.firebaseio.com"
-  });
+//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // a concise way to store Firestore and Firebase admin
